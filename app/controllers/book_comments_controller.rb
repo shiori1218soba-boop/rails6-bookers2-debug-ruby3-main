342337c75f11cd77dcda778fe7f3fd5480a926c1
shiori1218soba-boop <235053419+shiori1218soba-boop@users.noreply.github.com>
2025-11-08 12:19:36 +0000

    [Add] いいね・コメントの非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,15 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
     else
       @new_book = Book.new
       @book = book
-      @book_comment = comment
-      @book_comments = book.book_comments
+      @book_comment = @comment
+      @book_comments = @book.book_comments
       flash.now[:alert] = "コメントを入力してください。"
       render 'books/show'
     end
@@ -18,7 +17,6 @@ class BookCommentsController < ApplicationController
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
 
